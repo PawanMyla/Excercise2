ab46395aad18dd400bbbd947f3dbe0d0e3a982b8
PawanMyla <pavankumarmyla5@gmail.com>
2024-08-08 09:34:53 +0000

    Add files via upload

---
+++ Ex2/src/main/java/com/tcs/Ex2/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.tcs.Ex2;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    Employee findByName(String name);
+}
