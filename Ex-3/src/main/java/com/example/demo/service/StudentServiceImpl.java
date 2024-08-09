3ea0b6a8331769fa7e317d86a0f1d3c395659b76
PawanMyla <pavankumarmyla5@gmail.com>
2024-08-09 14:28:28 +0530

    Add files via upload

---
+++ Ex-3/src/main/java/com/example/demo/service/StudentServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Student;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+	@Override
+	public List<Student> getAllStudents() {
+		List<Student> students = new ArrayList<>();
+		students.add(new Student("John Doe" , "Robert Doe","2001-01-01","Male"));
+		students.add(new Student("Jane Doe" , "Robert Doe","2001-01-01","female"));
+		return students;
+		
+	}
+
+}
